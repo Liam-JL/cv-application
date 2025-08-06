0d7f44666ff7f1d718dac8ae32cdde7da77310d1
LiamJL <paledeepened@gmail.com>
2025-08-06 13:45:32 +0100

    Style Work History component

--- src/HistoryBlock.jsx
+++ src/HistoryBlock.jsx
@@ -1,15 +1,16 @@
 export function HistoryBlock({ active, id, title, company, location, dates, summary, handleEdit, handleSummaryEdit, handleDelete }) {
 
     return active ? (
-        <li>
-                <input name="title" type="text" value={title} onChange={e => handleEdit(id, "title", e.target.value)} />
-                <input name="company" type="text" value={company} onChange={e => handleEdit(id, "company", e.target.value)} />
-                <input name="location" type="text" value={location} onChange={e => handleEdit(id, "location", e.target.value)} />
-                <input name="dates" type="text" value={dates} onChange={e => handleEdit(id, "dates", e.target.value)} />
-                <ul>
+        <li className="history-block">
+                <input className="history-block__input history-block__title" name="title" type="text" value={title} onChange={e => handleEdit(id, "title", e.target.value)} />
+                <input className="history-block__input history-block__company" name="company" type="text" value={company} onChange={e => handleEdit(id, "company", e.target.value)} />
+                <input className="history-block__input history-block__dates" name="dates" type="text" value={dates} onChange={e => handleEdit(id, "dates", e.target.value)} />
+                <input className="history-block__input history-block__location" name="location" type="text" value={location} onChange={e => handleEdit(id, "location", e.target.value)} />
+                <ul className="history-block__summary-list">
                     {summary.map((point, index) => 
                         <li>
                             <input 
+                            className="history-block__input history-block__summary-point"
                             key={index}
                             type="text"
                             value={point}
@@ -21,13 +22,13 @@ export function HistoryBlock({ active, id, title, company, location, dates, summ
                 <button className="btn" onClick={() => handleDelete(id)}>Delete Block</button>
         </li>
     ) : (
-        <li>
-            <h3>{title}</h3>
-            <p>{company}</p>
-            <p>{location}</p>
-            <p>{dates}</p>
-            <ul>
-                {summary.map(point => <li>{point}</li>)}
+        <li className="history-block">
+            <h3 className="history-block__title">{title}</h3>
+            <p className="history-block__company">{company}</p>
+            <p className="history-block__dates">{dates}</p>
+            <p className="history-block__location">{location}</p>
+            <ul className="history-block__summary-list">
+                {summary.map(point => <li className="history-block__summary-point">{point}</li>)}
             </ul>
         </li>
     )
